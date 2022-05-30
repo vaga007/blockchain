98902788f321824d731de6a5efcab8755a02ad12
Raktima Saxena <raktima.saxena@gmail.com>
2022-05-30 06:39:07 +0530

    feat(blockchain):  Initial commit

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+export const GENESIS_BLOCK = {
+    nonce: 100,
+    previousBlockHash: "0",
+    hash: "0000",
+    transactions: [],
+    index: 1
+  };
+  
+ export const BITCOIN_LEADING_HASH_CHARACTERS = "0000";
+
+
+ // TODO: use constants to build api routes
+ export const TRANSACTION_PATHS = {
+  root: '/transaction',
+  broadcast: '/broadcast',
+  id: ':transactionId'
+
+ }
+
+ export const BLOCKS_PATHS = {
+   receiveNewBlock: '/receive-new-block'
+ }
+ 
